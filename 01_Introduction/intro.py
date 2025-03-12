7e8a4ad2273d359d90bfd9442b92a77370c1ef25
kellygattx <kelly.gatt.i84934@mcast.edu.mt>
2025-03-12 10:45:04 +0100

    Initial commit

---
+++ 01_Introduction/intro.py
@@ -0,0 +1,14 @@
+#create two variables; one storing your age, and the
+#other the minimum age (18). display you can drive
+# if your age is equal or greater than the minimum
+#otherwise display another message
+
+my_age = 19
+minimum_age = 18
+
+if my_age >= minimum_age:
+    print ('Your age is' my_age)
+    print("You can drive!")
+else:
+    print("You are not old enough to drive.")
+
